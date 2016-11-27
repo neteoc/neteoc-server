5515d53f896a02310ee111982963df2d43daed32
DarrellBailey <dbaile10@gmail.com>
2016-11-27 16:59:26 -0500

    updated swagger documentation for auth service

--- Source/Services/NetEOC.Auth/Controllers/UsersController.cs
+++ Source/Services/NetEOC.Auth/Controllers/UsersController.cs
@@ -30,25 +30,26 @@ namespace NetEOC.Auth.Controllers
         }
 
         [Authorize, HttpGet]
-        [SwaggerResponse(typeof(User))]
+        [SwaggerResponse(typeof(User), Description = "Get the currently logged in user.")]
         public async Task<ActionResult> Get()
         {
-            string authId = GetAuthIdFromContext();
+            Guid userId = GetUserIdFromContext();
 
-            User user = await UserService.GetByAuthId(authId);
+            User user = await UserService.GetById(userId);
 
             return Ok(user);
         }
 
         [Authorize, HttpGet("{id}")]
-        [SwaggerResponse(typeof(User))]
+        [SwaggerResponse(typeof(User), Description = "Get the user with the given id.")]
         public async Task<ActionResult> Get(Guid id)
         {
             return Ok(await UserService.GetById(id));
         }
 
         [Authorize, HttpPost]
-        [SwaggerResponse(typeof(User))]
+        [SwaggerResponse(typeof(User), Description = "Create/Validate a user. This should be called every time a user logs in as it will validate their authorization token and make sure Auth0 has updated user information.")]
+        [SwaggerResponse(401, typeof(User), Description = "The users token is either non existent, expired, or needs to be invalidated from Auth0.")]
         public async Task<ActionResult> Post([FromBody]User user)
         {
             user.AuthId = GetAuthIdFromContext();
@@ -81,7 +82,7 @@ namespace NetEOC.Auth.Controllers
         }
 
         [Authorize, HttpPut("{id}")]
-        [SwaggerResponse(typeof(User))]
+        [SwaggerResponse(typeof(User), Description = "Update the given user. not every value is required, the user object will be merged into the existing object.")]
         public async Task<ActionResult> Put(Guid id, [FromBody]User user)
         {
             user.AuthId = GetAuthIdFromContext();
@@ -101,7 +102,7 @@ namespace NetEOC.Auth.Controllers
         }
 
         [Authorize, HttpDelete("{id}")]
-        [SwaggerResponse(typeof(bool))]
+        [SwaggerResponse(typeof(bool), Description = "Delete the given user.")]
         public async Task<ActionResult> Delete(Guid id)
         {
             bool canDelete = id == GetUserIdFromContext();
@@ -117,7 +118,7 @@ namespace NetEOC.Auth.Controllers
         }
 
         [Authorize, HttpGet("{id}/organizations")]
-        [SwaggerResponse(typeof(Organization[]))]
+        [SwaggerResponse(typeof(Organization[]), Description = "Get all organizations that the given user is either an owner, administrator, or member of.")]
         public async Task<ActionResult> Organizations(Guid id)
         {
             Guid[] orgs = await UserService.GetUserOrganizations(id);
